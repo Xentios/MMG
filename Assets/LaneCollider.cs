8f90f987ccb9b4b1b43174dada257d24cb5c4d10
Xentios <xenq@hotmail.com>
2023-04-08 08:40:38 +0300

    Update LaneCollider.cs

--- Assets/LaneCollider.cs
+++ Assets/LaneCollider.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class LaneCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerExit2D(Collider2D collision)
     {       
         if (collision.CompareTag("ZomWick")) {
