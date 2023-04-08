f87278edf168c36d6e2ef20273fc97f4c89520c9
Xentios <xenq@hotmail.com>
2023-04-08 08:40:20 +0300

    Update LaneCollider.cs

--- Assets/LaneCollider.cs
+++ Assets/LaneCollider.cs
@@ -17,8 +17,7 @@ public class LaneCollider : MonoBehaviour
     }
 
     private void OnTriggerExit2D(Collider2D collision)
-    {
-        Debug.Log("Collision Dedected");
+    {       
         if (collision.CompareTag("ZomWick")) {
             var rb2D=collision.gameObject.GetComponent<Rigidbody2D>();
             rb2D.velocity = new Vector2(rb2D.velocity.x, 0f);
