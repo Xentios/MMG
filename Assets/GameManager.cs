7191eb8ac1e736cad77185d78b91a11e24764772
Xentios <xenq@hotmail.com>
2023-04-09 11:33:23 +0300

    Game Over Volume added

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Rendering;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -10,15 +12,38 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private TMP_Text gameTimerText;
+
+    [SerializeField]
+    private GameObject zomWick;
+
+    public Volume Volume;
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
         gameTimerInSeconds -= Time.deltaTime;
-        gameTimerText.text =" : "+ (int) gameTimerInSeconds + "";
+        gameTimerText.text = " : " + (int) gameTimerInSeconds + "";
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Game Over");
+        StartCoroutine(EndGameEffect());
+    }
+
+    IEnumerator EndGameEffect()
+    {
+        float duration = 1f;
+        float elapsedTime = 0;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            Volume.weight = elapsedTime;           
+            yield return null;
+        }
     }
 }
