a7f8c064d8a8b422e2ccea3a0e413f0bda8ff077
Xentios <xenq@hotmail.com>
2023-04-15 10:52:38 +0300

    Trying So

---
+++ Assets/Scripts/ScriptableObjects/TowerData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TowerData : ScriptableObject
+{
+    public float effectTimerLimit;      
+
+    public float offlineTowerTimerLimit;
+
+    public float recyleTimer;
+
+
+}
